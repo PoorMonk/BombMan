d6d0446759fc32454fadb9cd0aea92f8d55af7a5
PoorMonk <461874693@qq.com>
2020-02-29 09:35:35 +0800

    first commit

---
+++ Source/BombMan/BombManGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BombManGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOMBMAN_API ABombManGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
